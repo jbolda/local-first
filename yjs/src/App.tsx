2545da2f0ce4a3ea00627b5f0a03321efaac2af3
Jacob Bolda <me@jacobbolda.com>
2024-10-02 15:00:02 -0500

    spike out yjs implementation

---
+++ yjs/src/App.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+import * as Y from "yjs";
+
+function App({
+  ymap,
+  yarray,
+  idb,
+}: {
+  ymap: Y.Map<unknown>;
+  yarray: Y.Array<string>;
+  idb: any;
+}) {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("none");
+  const [lastUpdate, setLastUpdate] = useState(0);
+  if (idb)
+    idb.whenSynced.then(() => {
+      console.log("loaded data from indexed db");
+      setLastUpdate(Date.now());
+    });
+
+  useEffect(() => {
+    // observe changes of the sum
+    ymap.observe((event) => {
+      // print updates when the data changes
+      console.log({ ymapEvent: event });
+      setLastUpdate(Date.now());
+    });
+    yarray.observe((event) => {
+      // print updates when the data changes
+      console.log({ yarrayEvent: event });
+      setLastUpdate(Date.now());
+    });
+  }, []);
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    // setGreetMsg(await invoke("greet", { name }));
+    ymap.set(
+      name,
+      `Greetings ${name}, saying hi from a ymap at timestamp ${Date.now()}`
+    );
+    yarray.push([`greeted ${name}`]);
+  }
+
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+      <pre>{`${JSON.stringify(ymap.toJSON(), null, 2)}`}</pre>
+      <pre>{`${JSON.stringify(yarray.toJSON(), null, 2)}`}</pre>
+    </div>
+  );
+}
+
+export default App;
---
+++ yjs/src/main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+// import { WebsocketProvider } from 'y-websocket'
+// import { IndexeddbPersistence } from "y-indexeddb";
+
+const ydoc = new Y.Doc();
+const protocol = "wss"; // window.location.protocol === "https" ? "wss" : "ws";
+
+// this allows you to instantly get the (cached) documents data
+// const indexeddbProvider = new IndexeddbPersistence("greeter-list", ydoc);
+
+// Sync clients with the y-webrtc provider.
+const webrtcProvider = new WebrtcProvider("tauri-greeter", ydoc, {
+  signaling: [`${protocol}://signaling.yjs.dev`, `wss://sync.automerge.org`],
+  password: "optional-room-password",
+});
+webrtcProvider.on("status", (args) => console.log(args));
+webrtcProvider.on("peers", (args) => console.log(args));
+webrtcProvider.on("synced", (args) => console.log(args));
+
+// Sync clients with the y-websocket provider
+// const websocketProvider = new WebsocketProvider(
+//   'wss://demos.yjs.dev', 'count-demo', ydoc
+// )
+
+// array of numbers which produce a sum
+const ymap = ydoc.getMap("greeters");
+const yarray = ydoc.getArray<string>("greeted");
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+  <App ymap={ymap} yarray={yarray} idb={null} />
+  // </React.StrictMode>
+);
